b3ee45b945a8ca7166bf508ee951e62aa7393b89
Vlad <vladikhoncharuk@gmail.com>
2025-10-17 19:55:00 +0200

    commit

--- services/instance.ts
+++ services/instance.ts
@@ -3,11 +3,13 @@ import axios from 'axios'
 let baseURL: string;
 
 if (typeof window === "undefined") {
+    
     const vercelUrl = process.env.VERCEL_URL
         ? `https://${process.env.VERCEL_URL}`
         : "http://localhost:3000";
 
     baseURL = vercelUrl;
+
 } else {
     baseURL = "";
 }
